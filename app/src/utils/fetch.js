e29f600f52b4ac9fc3d582bbdac9368a133dd9a9
John <jun0205@live.com>
2026-03-04 14:46:17 +0800

    fix: exclude Cloudflare 5xx errors (520+) from showing HTML in toast

--- app/src/utils/fetch.js
+++ app/src/utils/fetch.js
@@ -36,7 +36,7 @@ axios.interceptors.response.use(
 				if (
 					error.response &&
 					error.response.status >= 400 &&
-					error.response.status < 600 &&
+					error.response.status < 520 &&
 					error.response.data
 				) {
 					errorMessage = i18n.t(error.response.data);
